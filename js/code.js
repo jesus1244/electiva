310de016fa87d1a2ba662e9a0d73d8a66c4642a0
cabiver <carlosvergel01@gmail.com>
2021-09-07 14:12:47 -0500

    fixed etiquetas marquee

--- js/code.js
+++ js/code.js
@@ -2,6 +2,15 @@
 const loding = document.getElementById("code.js--poner-none")
 const pagina = document.getElementById("code.js--quitar-none")
 const marquee = document.getElementById("code.js--marquee")
+const video = document.getElementById("code.js--video")
+const img = document.getElementById("code.js--img")
+
+console.log(video.naturalWidth)
+video.addEventListener("loaddata",e=>{
+    console.log("te doy la mano")
+    img.style.height = `${video.naturalHeight}px`
+})
+
 window.addEventListener("load",e=>{
     setTimeout(()=>{
     pagina.removeAttribute("hidden");
